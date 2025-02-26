9bc81d0f3d0471b1cb0616530095c9d5f7d049e8
Mojtaba/ <77284468+mojtaba313@users.noreply.github.com>
2025-02-26 19:28:41 +0330

    Update layout.tsx (resolve font error)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Settings from "../components/layout/Settings";
 
@@ -16,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
         <Settings/>
